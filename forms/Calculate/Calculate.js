534ef2739061fee244d19d1fbf18433cb5254fce
amandarosa15 <79384892+amandarosa15@users.noreply.github.com>
2021-03-03 20:11:12 -0600

    Initial commit

---
+++ forms/Calculate/Calculate.js
@@ -0,0 +1,7 @@
+/*
+var num1= parseInt(prompt("Insert one number"))
+var num2= parseInt(prompt("Insert another number"))
+var summedNumbers= Calculate(num1, num2)
+  
+  console.log(`The sum of ${num1} and ${num2} is ${summedNumbers}.`)
+*/
---
+++ forms/for/for.js
@@ -0,0 +1,10 @@
+/*
+let names=["Bob","Janet","Tom", "Erin", "Lucy", "Alyssa","Dustin"]
+for(i=0; i<names.length; i++)
+  console.log(names[i])
+
+function Calculate(n1, n2){
+  return n1 + n2
+  } 
+  
+*/
